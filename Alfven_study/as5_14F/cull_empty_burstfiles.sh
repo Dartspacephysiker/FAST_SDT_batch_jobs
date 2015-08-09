c76a2aa074f6710e69a9e4d3a59be03103d38109
DartSpacePhysiker <hammerhatch@gmail.com>
2015-08-08 20:39:42 -0400

    About to do some mods to as5 to make burst-mode fields data a possibility. We'll see...

--- Alfven_study/as5_14F/cull_empty_burstfiles.sh
+++ Alfven_study/as5_14F/cull_empty_burstfiles.sh
@@ -9,8 +9,9 @@ for file in ${CULLDIR}/Dartmouth_as5_dflux_*; do
     this="`wc -l ${file} | awk '{print $1}'`"
     [ ${this} -eq "42" ] && \
 	[ "`ls -la ${file} | awk '{print $5}'`" -eq "2112" ] && \
-	echo "Deleting ${file}..." && mv ${file} ${file}_BAD && rm ${file} -v && echo "${file} ${this}" >> various_textfiles_and_notes/culled_burstfiles.txt 
-    
+	echo "Deleting ${file}..." && \
+	rm ${file} -v && \
+	echo "${file} ${this}" >> various_textfiles_and_notes/culled_burstfiles.txt 
 done
 
 echo "Bro!!"
