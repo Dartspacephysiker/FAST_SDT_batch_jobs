9717db1000cf98b5a4f99f28c376a26b15df7549
Spencer Hatch (Vær så godt) <hammerhatch@gmail.com>
2018-05-01 13:19:25 -0400

    Some improvements to slapTogetherStrangewayAndKappaSumfiles.sh

--- kappas_Maxwellians_for_inverted_Vs/bash_scripts/slapTogetherStrangewayAndKappaSumfiles.sh
+++ kappas_Maxwellians_for_inverted_Vs/bash_scripts/slapTogetherStrangewayAndKappaSumfiles.sh
@@ -4,10 +4,16 @@
 # NOTE: THE ORDER OF ORBIT PLOTS CATTED IS NOT NUMERICAL; IT IS BASED ON THE TIME OF CREATION (hence ls -lahtr)
 
 
-dir=/SPENCEdata/software/sdt/batch_jobs/plots/20180425/kappa_fits/
-PDFSUFF="_to300"
+dir=/SPENCEdata/software/sdt/batch_jobs/plots/20180426/kappa_fits/
 NPLOTSTOCAT=100
-startOrb=5683
+
+# 2018/04/27
+# PDFSUFF="_to300"
+# startOrb=5683
+
+# 2018/05/01
+PDFSUFF="_to400"
+startOrb=8217
 
 if [ ! -d "${dir}" ]; then
     echo "Can't! N'existe pas: ${dir}";
@@ -17,21 +23,33 @@ fi
 cd ${dir}
 CWD=$(pwd);
 
-mapfile -t myArray < <(ls -lahtr | awk '{print $9}' | awk -v startOrb=${startOrb} -v nPlotsToCat=${NPLOTSTOCAT} 'BEGIN { orbDir="Orbit_" startOrb ; print orbDir; }; $0 ~ orbDir {for(i=1; i<=100; i++) {getline; print}}')
+mapfile -t myArray < <(ls -lAhtr --ignore='*pdf' 2>/dev/null | awk '{print $9}' | awk -v startOrb=${startOrb} -v nPlotsToCat=${NPLOTSTOCAT} 'BEGIN { orbDir="Orbit_" startOrb ; print orbDir; }; $0 ~ orbDir {for(i=1; i<=nPlotsToCat; i++) {getline; print}}')
 
 kappArr=();
 swayArr=();
 
 for bro in ${myArray[@]}; do
-    orb=${bro##Orbit_}; echo ${orb};
-    if [ -n "$(ls ${bro}/Kappa_summary*)" ]; then
-	echo "${orb}: K!"; kappArr=(${kappArr[@]} $(ls ${bro}/Kappa_summary*) )
+    orb=${bro##Orbit_};
+    printf "${orb}:";
+    if [ -n "$(ls ${bro}/Kappa_summary* 2>/dev/null)" ]; then
+	printf " K!"; kappArr=(${kappArr[@]} $(ls ${bro}/Kappa_summary*) )
     fi
-    if [ -n "$(ls ${bro}/Strangeway_summary*)" ]; then
-	echo "${orb}: S!"; swayArr=(${swayArr[@]} $(ls ${bro}/Strangeway_summary*) )
+    if [ -n "$(ls ${bro}/Strangeway_summary* 2>/dev/null)" ]; then
+	printf " S!"; swayArr=(${swayArr[@]} $(ls ${bro}/Strangeway_summary*) )
     fi
+    printf "\n"
     cd ${CWD}
 done
 
-# gs -sDEVICE=pdfwrite -dEPSFitPage -dNOPAUSE -dBATCH -dSAFER -sOutputFile=sWayOrbits${PDFSUFF}.pdf ${swayArr[@]}
-# gs -sDEVICE=pdfwrite -dEPSFitPage -dNOPAUSE -dBATCH -dSAFER -sOutputFile=kappaOrbits${PDFSUFF}.pdf ${kappArr[@]}
+if [ -e "sWayOrbits${PDFSUFF}.pdf" ]; then
+    echo "About to overwrite sWayOrbits${PDFSUFF}.pdf--ain't gonna do it!"
+    exit
+fi
+
+if [ -e "kappaOrbits${PDFSUFF}.pdf" ]; then
+    echo "About to overwrite kappaOrbits${PDFSUFF}.pdf--ain't gonna do it!"
+    exit
+fi
+
+gs -sDEVICE=pdfwrite -dEPSFitPage -dNOPAUSE -dBATCH -dSAFER -sOutputFile=sWayOrbits${PDFSUFF}.pdf ${swayArr[@]}
+gs -sDEVICE=pdfwrite -dEPSFitPage -dNOPAUSE -dBATCH -dSAFER -sOutputFile=kappaOrbits${PDFSUFF}.pdf ${kappArr[@]}
